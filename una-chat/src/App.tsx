53585206231736ccdcdff735869354065cf56dcc
BrandonJafeth <brandon.carrillo.alvarez@est.una.ac.cr>
2025-10-17 22:54:15 -0600

    feat/initial commit

---
+++ una-chat/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <div className='h-screen flex justify-center items-center '>
+     <h1 className='text-white text-center text-6xl '> UNA CHAT </h1>
+    </div>
+    </>
+  )
+}
+
+export default App
